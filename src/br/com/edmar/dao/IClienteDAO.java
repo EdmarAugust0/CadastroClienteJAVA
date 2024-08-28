8e4dfcdaad900f3cd72fad9864b9b560ec9581bb
EdmarAugust0 <edmar.contato149@gmail.com>
2024-08-28 14:36:28 -0300

    :tada: Inicial commit

---
+++ src/br/com/edmar/dao/IClienteDAO.java
@@ -0,0 +1,18 @@
+package br.com.edmar.dao;
+
+import java.util.Collection;
+
+import br.com.edmar.domain.Cliente;
+
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
